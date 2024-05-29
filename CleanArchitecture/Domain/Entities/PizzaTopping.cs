af8fd0b1843282bf6e0a16c6cf5950d154c6458d
Gonzalo Flores Chuchon <gonzalo.flores@ext.semicrol.com>
2024-05-29 17:06:36 +0200

    Estructura de Carpetas y archivos para CleanArchitecture

---
+++ CleanArchitecture/Domain/Entities/PizzaTopping.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAccessBlazor;
+
+public class PizzaTopping
+{
+    public Topping Topping { get; set; }
+
+    public int ToppingId { get; set; }
+
+    public int PizzaId { get; set; }
+}
